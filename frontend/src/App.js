0c258e02f3378dca3a2214366cae7400035c0aed
Anish De <anishde85@gmail.com>
2021-05-29 01:19:10 -0400

    appjs fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+import RoomContainer from './components/room_container'
+import Landing from './components/landing'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+        <Switch>
+        <Route exact path="/" component={Landing}/>
+        <Route path="/meeting/:id" component={RoomContainer}/>
+        <Redirect to="/"/>
+      </Switch>
+    </Router>
     </div>
   );
 }
