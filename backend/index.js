bc99e74b0edc575ca42cdde5005db393320e2529
Snehasish Dhar <dsnehasish74@gmail.com>
2021-05-29 15:47:42 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ io.on('connection', (socket) => {
         socket.broadcast.emit('message', 'A user has been Disconnected');
     })
  });
- const port = 3000;
+ const port = 8000;
 server.listen(port,()=>{
     console.log(`The Server is listening on port ${port}`)
 });
